b35aa04bcae88e811ffc3488a7dee8d78cd40023
anmolsinghchhabra <anmol.singh@jamsfy.com>
2021-10-18 01:20:32 +0530

    my first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd54597add292dda49785bd78dbf64ea3'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b7aaaefa-2683-41c5-93a6-055b0132799b'
+};
